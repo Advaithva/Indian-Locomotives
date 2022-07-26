65767897a4916408386798eccb553a0260c03cc5
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-07-26 10:58:50 +0530

    Update mmmm.js

--- mmmm.js
+++ mmmm.js
@@ -2,3 +2,8 @@ function DONE() {
   alert("YDM 4 LOCOMOTIVE") 
   alert("It is a meter gauge locomotive") 
 }
+
+function D2() {
+  alert("WDG 3A LOCOMOTIVE") 
+  alert("It is a broad gauge locomotive") 
+}
