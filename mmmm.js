fc706ac2c96bd88493d03d33cf852533ffc94199
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-07-26 16:39:13 +0530

    Update mmmm.js

--- mmmm.js
+++ mmmm.js
@@ -9,32 +9,32 @@ function DTWO() {
   alert("It is a broad gauge locomotive") 
 }
 function DTHREE() {
-  alert("YDM 4 LOCOMOTIVE") 
-  alert("It is a meter gauge locomotive") 
+  alert("WDG 4 LOCOMOTIVE") 
+  alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 96 km/h")
 }
 
 function DFOUR() {
-  alert("WDG 3A LOCOMOTIVE") 
+  alert("WDM 2 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
 }
 function DFIVE() {
-  alert("YDM 4 LOCOMOTIVE") 
-  alert("It is a meter gauge locomotive") 
+  alert("WDM 3A LOCOMOTIVE") 
+  alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 96 km/h")
 }
 
 function DSIX() {
-  alert("WDG 3A LOCOMOTIVE") 
+  alert("WDP 1 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
 }
 function DSEVEN() {
-  alert("YDM 4 LOCOMOTIVE") 
-  alert("It is a meter gauge locomotive") 
+  alert("WDP 3A LOCOMOTIVE") 
+  alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 96 km/h")
 }
 
 function DEIGHT() {
-  alert("WDG 3A LOCOMOTIVE") 
+  alert("WDP 4 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
 }
