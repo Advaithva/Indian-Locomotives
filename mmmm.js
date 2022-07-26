d7de88ab98eec3d290ec26d5b8ac00b4979d45d2
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-07-26 17:09:39 +0530

    Update mmmm.js

--- mmmm.js
+++ mmmm.js
@@ -2,50 +2,59 @@ function DONE() {
   alert("YDM 4 LOCOMOTIVE") 
   alert("It is a meter gauge locomotive") 
   alert("Maximum speed: 96 km/h")
+  alert("Power output: 4,500 hp (3,360 kW)")
 }
 
 function DTWO() {
   alert("WDG 3A LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 105 km/h")
+    alert("Power output: 4,500 hp (3,360 kW)")
 }
 function DTHREE() {
   alert("WDG 4 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 105 km/h")
+    alert("Power output: 4,500 hp (3,360 kW)")
 }
 
 function DFOUR() {
   alert("WDM 2 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 96 km/h")
+    alert("Power output: 4,500 hp (3,360 kW)")
 }
 function DFIVE() {
   alert("WDM 3A LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 96 km/h")
+  alert("Power output: 4,500 hp (3,360 kW)")
 }
 
 function DSIX() {
   alert("WDP 1 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 96 km/h")
+    alert("Power output: 4,500 hp (3,360 kW)")
 }
 function DSEVEN() {
   alert("WDP 3A LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 96 km/h")
+    alert("Power output: 4,500 hp (3,360 kW)")
 }
 
 function DEIGHT() {
   alert("WDP 4 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 180 km/h")
+    alert("Power output: 4,500 hp (3,360 kW)")
 }
 
 function DNINE() {
   alert("WDP 4D LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 135 km/h")
+    alert("Power output: 4,500 hp (3,360 kW)")
 }
 
