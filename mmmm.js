a11643df01308c3e0cc7daf4c3e24a4ce80732ff
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-07-27 14:52:49 +0530

    Update mmmm.js

--- mmmm.js
+++ mmmm.js
@@ -58,6 +58,9 @@ function DNINE() {
     alert("Power output: 4,500 hp (3,360 kW)")
 }
 
+
+
+
 function EONE() {
   alert("WAG 5 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
@@ -65,53 +68,53 @@ function EONE() {
   alert("Power output: 1,400 hp (1,000 kW)")
 }
 
-function DTWO() {
+function ETWO() {
   alert("WAG 7 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 105 km/h")
     alert("Power output:3100 HP")
 }
-function DTHREE() {
+function ETHREE() {
   alert("WAG 9 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 105 km/h")
     alert("Power output: 4,500 hp (3,360 kW)")
 }
 
-function DFOUR() {
+function EFOUR() {
   alert("WAG 12 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 120 km/h")
     alert("Power output: 2,600 hp (1,900 kW)")
 }
-function DFIVE() {
+function EFIVE() {
   alert("WAM 4 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 120 km/h")
   alert("Power output: 3100 HP")
 }
 
-function DSIX() {
+function ESIX() {
   alert("WCAM 3 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 140 km/h")
     alert("Power output: 2,300 hp (1,700 kW")
 }
-function DSEVEN() {
+function ESEVEN() {
   alert("WAP 4 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 160 km/h")
     alert("Power output: 3100 HP")
 }
 
-function DEIGHT() {
+function EEIGHT() {
   alert("WAP 5 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 180 km/h")
     alert("Power output: 4,000 hp (2,980 kW)");
 }
 
-function DNINE() {
+function ENINE() {
   alert("WAP 7 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 135 km/h")
