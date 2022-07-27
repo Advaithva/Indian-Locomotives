58cf43c394438aafbedda40f4bb4a404f5a436e1
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-07-27 14:49:43 +0530

    Update mmmm.js

--- mmmm.js
+++ mmmm.js
@@ -58,61 +58,61 @@ function DNINE() {
     alert("Power output: 4,500 hp (3,360 kW)")
 }
 
-function DONE() {
-  alert("YDM 4 LOCOMOTIVE") 
-  alert("It is a meter gauge locomotive") 
+function EONE() {
+  alert("WAG 5 LOCOMOTIVE") 
+  alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 96 km/h")
   alert("Power output: 1,400 hp (1,000 kW)")
 }
 
 function DTWO() {
-  alert("WDG 3A LOCOMOTIVE") 
+  alert("WAG 7 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 105 km/h")
     alert("Power output:3100 HP")
 }
 function DTHREE() {
-  alert("WDG 4 LOCOMOTIVE") 
+  alert("WAG 9 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 105 km/h")
     alert("Power output: 4,500 hp (3,360 kW)")
 }
 
 function DFOUR() {
-  alert("WDM 2 LOCOMOTIVE") 
+  alert("WAG 12 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 120 km/h")
     alert("Power output: 2,600 hp (1,900 kW)")
 }
 function DFIVE() {
-  alert("WDM 3A LOCOMOTIVE") 
+  alert("WAM 4 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 120 km/h")
   alert("Power output: 3100 HP")
 }
 
 function DSIX() {
-  alert("WDP 1 LOCOMOTIVE") 
+  alert("WCAM 3 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 140 km/h")
     alert("Power output: 2,300 hp (1,700 kW")
 }
 function DSEVEN() {
-  alert("WDP 3A LOCOMOTIVE") 
+  alert("WAP 4 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive") 
   alert("Maximum speed: 160 km/h")
     alert("Power output: 3100 HP")
 }
 
 function DEIGHT() {
-  alert("WDP 4 LOCOMOTIVE") 
+  alert("WAP 5 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 180 km/h")
     alert("Power output: 4,000 hp (2,980 kW)");
 }
 
 function DNINE() {
-  alert("WDP 4D LOCOMOTIVE") 
+  alert("WAP 7 LOCOMOTIVE") 
   alert("It is a broad gauge locomotive")  
   alert("Maximum speed: 135 km/h")
     alert("Power output: 4,500 hp (3,360 kW)")
